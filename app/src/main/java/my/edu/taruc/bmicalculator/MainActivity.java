c54705629e73df32e51b3ca30e64128bab1a5ec7
keatt <laubk-pa16@student.tarc.edu.my>
2018-12-18 13:13:00 +0800

    Second commit

--- app/src/main/java/my/edu/taruc/bmicalculator/MainActivity.java
+++ app/src/main/java/my/edu/taruc/bmicalculator/MainActivity.java
@@ -5,50 +5,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String BMI = "my.edu.taruc.bmicalculator";
-    private EditText editWeight;
-    private EditText editHeight;
 
-
+    public static final String BMI = "my.edu.taruc.bmicalculator.MESSAGE";
+    private TextView Textweight,Textheight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editWeight = findViewById(R.id.editWeight);
-        editHeight = findViewById(R.id.editHeight);
+
+        Textweight=findViewById(R.id.editTextWeight);
+        Textheight=findViewById(R.id.editTextHeight);
     }
-
-    public void sendMessage(View view) {
-        String stringMessage;
-
-        double height, weight, bmi;
-
-        if (TextUtils.isEmpty(editWeight.getText())) {
-            editWeight.setError("Please enter your weight");
+    public void calculateBMI(View view){
+        double weight,height;
+        double bmi;
+        if(TextUtils.isEmpty(Textweight.getText())){
+            Textweight.setError("Please enter your weight");
             return;
-
-        } else if (TextUtils.isEmpty(editHeight.getText())) {
-            editHeight.setError("Please enter your height");
-            return;
-
         }
+        if(TextUtils.isEmpty(Textheight.getText())){
+            Textheight.setError("Please enter your height");
+            return;
+        }
+        weight=Double.parseDouble(Textweight.getText().toString());
+        height=Double.parseDouble(Textheight.getText().toString())/100;
+        bmi=weight/(height*height);
 
-        weight = Double.parseDouble(editWeight.getText().toString());
-        height = Double.parseDouble(editHeight.getText().toString());
-        bmi = weight/(height*height);
-
-
-
-        Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra(BMI, bmi);
+        Intent intent=new Intent( this,Result.class);
+        intent.putExtra(BMI,bmi);
         startActivity(intent);
 
+
+
     }
-
-
-
-
 }
